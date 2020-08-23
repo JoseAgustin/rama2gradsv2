6063d21c5bce9b67bac70d351a7f26e2597508ef
Agustin Garcia <agustin@atmosfera.unam.mx>
2020-08-22 22:18:07 -0500

    output test

--- util/prueba.sh
+++ util/prueba.sh
@@ -22,12 +22,21 @@ cat << End_of_Scr > prueba.gs
 'set lat 19.15 19.75'
 'set lon -99.40 -98.80'
 'set gxout shaded'
-'set mpdset estados'
+'set mpdset util/estados'
 'set map 15 1 7'
 'd oacres(a.2,stnmax(t,t=1,t=240),10,8,6,4,2)'
-'d t'
-'draw title Temperatura Maxima 10 dias'
+'d stnmax(t,t=1,t=240)'
+'draw title Max Temperature in 10 dias'
 'cbarn'
+'gxprint ftemp.png x768 y1024 white'
+'clear'
+'d oacres(a.2,stnmax(o3,t=1,t=240),10,8,6,4,2)'
+'d stnmax(o3,t=1,t=240)'
+'draw title Max ozone in 10 dias'
+'cbarn'
+'gxprint fozone.png x768 y1024 white'
+'clear'
+
 
 End_of_Scr
 cat << End_dumm > dummy.ctl
